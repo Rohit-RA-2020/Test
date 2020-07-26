cf2d154e693d52514a2bea40bf71e7e7a6106d4e
Rohit-RA-2020 <ranjanrohit812@gmail.com>
2020-07-26 18:12:38 +0530

    Commited javascript file

---
+++ Variables.js
@@ -0,0 +1,23 @@
+const uid = "abc123";
+
+var fullName = "Rohit ranjan";
+var email = "ranjanrohit12@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// fullname = prompt();
+
+// console.log(uid);
+// console.log("Full name is: " + fullName);
+// console.log(email);
+
+console.log(`
+    With Unique ID: ${uid}
+    User is : ${fullName}
+    and his email is: ${email}
+    and uses the password: ${password}
+
+
+`);
